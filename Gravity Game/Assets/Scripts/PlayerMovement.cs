668f0bdc9e1500c0bc0efb311ac2aa7b32bd3081
Alex Pritchard <alexpritc@users.noreply.github.com>
2019-11-12 23:24:16 +0000

    Prototype gravity gun.

---
+++ Gravity Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed = 7.5f;
+    private float distanceToGround = 1.2f;
+    private float gravity = -9.85f;
+    private float jumpHeight = 2f;
+
+    private Vector3 velocity;
+    private Vector3 down;
+
+    private CharacterController cc;
+
+    private bool isGrounded;
+
+    private RaycastHit hit;
+
+    void Start()
+    {
+        cc = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame.
+    void FixedUpdate()
+    {
+        down = transform.TransformDirection(Vector3.down);
+
+        Debug.DrawRay(transform.position, down, Color.red);
+
+        if (Physics.Raycast(transform.position, down, out hit, distanceToGround))
+        {
+            Debug.DrawRay(transform.position, down * hit.distance, Color.yellow);
+            isGrounded = true;
+            velocity.y = 0f;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // Local movement according to player's rotation.
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        cc.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y += Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        cc.Move(velocity * Time.deltaTime);
+
+        // Play footstep sounds here.
+    }
+}
---
+++ Gravity Game/Assets/Scripts/PlayerRayCast.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRayCast : MonoBehaviour
+{
+    public GameObject prefab;
+
+    private int groundMask = 8;
+
+    private RaycastHit hit;
+
+    private Vector3 fwd;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            fwd = transform.TransformDirection(Vector3.forward);
+
+            Debug.DrawRay(transform.position, fwd, Color.red);
+
+            if (Physics.Raycast(transform.position, fwd, out hit, Mathf.Infinity))
+            {
+                if (hit.collider.gameObject.layer == groundMask)
+                {
+                    Debug.DrawRay(transform.position, fwd * hit.distance, Color.yellow);
+                    SpawnBlackhole(hit.point);
+                }
+            }
+        }
+    }
+
+    void SpawnBlackhole(Vector3 location)
+    {
+        if (Blackhole.currentBlackhole == null)
+        {
+            Blackhole.currentBlackhole = Instantiate(prefab, location, prefab.transform.rotation);
+        }
+        else
+        {
+            Destroy(GameObject.Find("Blackhole(Clone)"));
+            Blackhole.currentBlackhole = Instantiate(prefab, location, prefab.transform.rotation);
+        }
+    }
+}
