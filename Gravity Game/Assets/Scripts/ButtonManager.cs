60d8476c700dfcb52801ff45a2182648231d8cad
Alex Pritchard <alexpritc@users.noreply.github.com>
2019-11-13 20:19:43 +0000

    Added prototype puzzle 01 scene.

---
+++ Gravity Game/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public static bool isLookingAtButton;
+
+    public GameObject[] spawnPoints;
+    public GameObject prefab;
+
+    private int numberOfTimesPressed = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLookingAtButton && Input.GetKeyUp(KeyCode.E))
+        {
+            Debug.Log("Pressing Button");
+            if (numberOfTimesPressed < spawnPoints.Length)
+            {
+                if (Blackhole.currentBlackhole == null)
+                {
+                    Blackhole.currentBlackhole = Instantiate(prefab, spawnPoints[numberOfTimesPressed].transform.position, prefab.transform.rotation);
+                }
+                else
+                {
+                    Destroy(GameObject.Find("Blackhole(Clone)"));
+                    Blackhole.currentBlackhole = Instantiate(prefab, spawnPoints[numberOfTimesPressed].transform.position, prefab.transform.rotation);
+                }
+            }
+            numberOfTimesPressed++;
+
+            if (numberOfTimesPressed == 3)
+            {
+                numberOfTimesPressed = 0;
+            }
+        }
+    }
+}
--- Gravity Game/Assets/Scripts/PlayerRayCast.cs
+++ Gravity Game/Assets/Scripts/PlayerRayCast.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class PlayerRayCast : MonoBehaviour
 {
+    public static bool hasPlayerGotBlackholeGun = false;
+
     public GameObject prefab;
 
     private int groundMask = 8;
+    private int buttonMask = 12;
 
     private RaycastHit hit;
 
@@ -15,12 +18,38 @@ public class PlayerRayCast : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        fwd = transform.TransformDirection(Vector3.forward);
+
+        Debug.DrawRay(transform.position, fwd, Color.red);
+
+        InteractWithButton();
+
+        if (hasPlayerGotBlackholeGun)
+        {
+            UseBlackholeGun();
+        }
+    }
+
+    void InteractWithButton()
+    {
+        if (Physics.Raycast(transform.position, fwd, out hit, Mathf.Infinity))
+        {
+            if (hit.collider.gameObject.layer == buttonMask)
+            {
+                Debug.DrawRay(transform.position, fwd * hit.distance, Color.yellow);
+                ButtonManager.isLookingAtButton = true;
+            }
+            else
+            {
+                ButtonManager.isLookingAtButton = false;
+            }
+        }
+    }
+
+    void UseBlackholeGun()
+    {
         if (Input.GetMouseButtonDown(0))
         {
-            fwd = transform.TransformDirection(Vector3.forward);
-
-            Debug.DrawRay(transform.position, fwd, Color.red);
-
             if (Physics.Raycast(transform.position, fwd, out hit, Mathf.Infinity))
             {
                 if (hit.collider.gameObject.layer == groundMask)
