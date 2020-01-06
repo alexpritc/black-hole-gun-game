fdac9872ef959d74da97e3389132d03b8c88e101
Alex Pritchard <alexpritc@users.noreply.github.com>
2020-01-06 03:28:59 +0000

    Huge map update!

---
+++ Gravity Game/Assets/Scripts/PuzzleManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleManager : MonoBehaviour
+{
+    // This script goes on the activator.
+
+    public GameObject cube;
+    public GameObject goal;
+
+    public GameObject newPos;
+    private GameObject oldPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldPos = goal;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == cube)
+        {
+            goal.transform.position = newPos.transform.position;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject == cube) {
+            goal.transform.position = oldPos.transform.position;
+        }
+    }
+}
